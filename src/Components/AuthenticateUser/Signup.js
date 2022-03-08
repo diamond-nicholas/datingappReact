13c14d2169e61ec0088a957838f92503edbfcbed
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-08 05:43:59 +0100

    complete banner for dating app home page

--- src/Components/AuthenticateUser/Signup.js
+++ src/Components/AuthenticateUser/Signup.js
@@ -14,6 +14,43 @@ const Signup = () => {
           </li>
         </ul>
       </header>
+      <section className='mainContent'>
+        <article>
+          <div className='leftContainer'>
+            <h1>
+              MyDatingAPP is a safe <br /> and comfortable place to find your
+              love
+            </h1>
+            <p>Find a relationship on your own terms</p>
+          </div>
+          <div className='rightContainer'>
+            <form>
+              <h3>Join Free</h3>
+              <div className='email'>
+                <input type='email' placeholder='E-mail' />
+              </div>
+              <div className='passwork'>
+                <input type='password' placeholder='Password' />
+              </div>
+              <div className='name'>
+                <input type='text' placeholder='name' />
+              </div>
+              <div className='city'>
+                <input type='text' placeholder='City' />
+              </div>
+              <div className='dob'>
+                <input type='date' placeholder='Date of birth' />
+              </div>
+              <div className='gender'>
+                <input type='text' placeholder='Gender' />
+              </div>
+              <div className='join'>
+                <button type='submit'>Join</button>
+              </div>
+            </form>
+          </div>
+        </article>
+      </section>
     </div>
   );
 };
