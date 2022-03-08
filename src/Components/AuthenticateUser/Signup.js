a5307332ca62a35b8389bb0d5c31c9c25ba99dc2
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-08 04:57:57 +0100

    add header for signup component

--- src/Components/AuthenticateUser/Signup.js
+++ src/Components/AuthenticateUser/Signup.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Signup.scss';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className='signup'>
+      <header>
+        <ul>
+          <li>MyDatingAPP</li>
+          <li className='second'>
+            <LockOpenIcon />
+            LOG IN
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 };
 
 export default Signup;
