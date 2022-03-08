35926560060805bd911e581ae612bfc3ea5c0a7c
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-08 04:36:25 +0100

    add react router dom to project, created signup component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Main from './Pages/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './Components/AuthenticateUser/Signup';
 
 function App() {
   return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Signup />} />
+        <Route path='/home' element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
