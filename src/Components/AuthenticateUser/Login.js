92b819dde0669e11b5d7d5c31652824acabc5a54
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-22 10:38:40 +0100

    add login functionality for users

--- src/Components/AuthenticateUser/Login.js
+++ src/Components/AuthenticateUser/Login.js
@@ -1,17 +1,93 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import './Login.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { loginRoute } from '../Utilis/APIroutes';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const toastOptions = {
+    position: 'top-right',
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'light',
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('date-app-user')) {
+      navigate('/login');
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      //call apis
+      const { email, password } = values;
+
+      const { data } = await axios.post(loginRoute, {
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.message, toastOptions);
+      }
+
+      if (data.status === true) {
+        localStorage.setItem('date-app-user', JSON.stringify(data.user));
+        console.log(data.user);
+        navigate('/search');
+      }
+    }
+  };
+
+  const handleValidation = () => {
+    const { email, password } = values;
+    if (email === '') {
+      toast.error('Please enter a valid email', toastOptions);
+      return false;
+    } else if (!password) {
+      toast.error('Please enter password', toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className='login'>
       <section className='wrapper'>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <h2>Login</h2>
           <div className='email'>
-            <input type='email' placeholder='Email' />
+            <input
+              type='email'
+              placeholder='Email'
+              name='email'
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className='password'>
-            <input type='password' placeholder='Password' />
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className='forgetPassword'>
             <p>Join free</p>
@@ -22,6 +98,7 @@ const Login = () => {
           </div>
         </form>
       </section>
+      <ToastContainer />
     </div>
   );
 };
--- src/Components/Utilis/APIroutes.js
+++ src/Components/Utilis/APIroutes.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
-const APIroutes = () => {
-  const host = 'http://localhost:8001';
 
-  const registerRoute = `${host}/api/v1/users/get`;
-  const loginRoute = `${host}/api/v1/users/signin`;
-};
+const host = 'http://localhost:8001';
 
-export default APIroutes;
+export const registerRoute = `${host}/api/v1/users/get`;
+export const loginRoute = `${host}/api/v1/users/signin`;
