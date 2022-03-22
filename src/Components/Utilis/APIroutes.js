a7773a2a84c7b5575dcf049da3f3405bd4331fe5
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-22 09:46:09 +0100

    add utils folders for api routes

---
+++ src/Components/Utilis/APIroutes.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+const APIroutes = () => {
+  const host = 'http://localhost:8001';
+
+  const registerRoute = `${host}/api/v1/users/get`;
+  const loginRoute = `${host}/api/v1/users/signin`;
+};
+
+export default APIroutes;
